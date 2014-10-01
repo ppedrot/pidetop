22c71a2a5335d0dd8ac7f7deb9188e0b1f019308
Carst Tankink <carst.tankink@inria.fr>
2014-10-01 11:29:43 +0200

    PIDE: print AST of commands added.
    After adding the commands to STM, PIDE will immediately report the AST
    of the added command (with the execution ID of said command).
    
    Some of the AST reports can be improved with better locations.

--- pide_document.ml
+++ pide_document.ml
@@ -166,6 +166,8 @@ let add task_queue exec_id tip edit_id text =
           Xml_datatype.Element ("running", [], [])])];
   try
     ignore(Stm.add ~newtip:exec_id ~ontop:tip true edit_id text);
+    let ast = Stm.print_ast exec_id in
+    Coq_output.report (Position.id_only (Stateid.to_string exec_id)) [ast];
     Some exec_id
   with e when Errors.noncritical e -> None))) task_queue;
   exec_id
