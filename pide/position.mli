b4fbc9f4583395bbbf948a2cf9af15bed13dcdb1
Carst Tankink <carst.tankink@inria.fr>
2014-10-17 15:51:53 +0200

    Remark for future refactor.

--- pide/position.mli
+++ pide/position.mli
@@ -4,6 +4,9 @@ val make: int -> int -> t
 val none: t
 val id_only: string -> t
 val properties_of: t -> Properties.t
+
+(* These need to move to Markup. *)
 val markup: t -> Markup.t -> Markup.t
 type report = t * Markup.t
+
 val reported_text: report -> string -> string
