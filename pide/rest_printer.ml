40e4df40938d1ee8764daebc0d458e4862d08ff4
Carst Tankink <carst.tankink@inria.fr>
2014-12-15 10:59:55 +0100

    Warning when adding an axiom.
    The warning message could be improved.

--- pide/rest_printer.ml
+++ pide/rest_printer.ml
@@ -1,14 +1,18 @@
 module Rest_printer_s = struct
   let can_print = function
-    | Feedback.Message _ | Feedback.Processed -> true
+    | Feedback.Message _ | Feedback.Processed | Feedback.AddedAxiom -> true
     | _ -> false
 
   let make_pos = function
-    | Feedback.Message _ | Feedback.Processed -> Position.id_only
+    | Feedback.Message _ | Feedback.Processed | Feedback.AddedAxiom ->
+        Position.id_only
     | _ -> raise Pide_printer.Unhandled
 
    let make_body _ = function
     | Feedback.Processed -> Some(Coq_markup.status_finished_element)
+    | Feedback.AddedAxiom ->
+        Some(Xml_datatype.Element(Coq_markup.warningN, [],
+          (Pide_xml.Encode.string "Axiom added.")))
     | Feedback.Message { Feedback.message_content = s } ->
         Some(Xml_datatype.Element(Coq_markup.writelnN, [],
           Pide_xml.Encode.string s))
@@ -18,6 +22,7 @@ module Rest_printer_s = struct
   let output_function = function
     | Feedback.Processed -> Coq_output.status
     | Feedback.Message _ -> Coq_output.writeln ~props:props
+    | Feedback.AddedAxiom -> Coq_output.warning_msg ?props:None
     | _ -> raise Pide_printer.Unhandled
 end
 
