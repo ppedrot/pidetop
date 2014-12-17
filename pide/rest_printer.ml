45a584f98d157fe9f66a38a25a6eb94c4e692f80
Carst Tankink <carst.tankink@inria.fr>
2014-12-17 11:09:17 +0100

    Ported rest_printer implementation to Printer_spec

--- pide/rest_printer.ml
+++ pide/rest_printer.ml
@@ -1,13 +1,11 @@
-let rest_printer : (module Pide_printer.Printer) = (module struct
+module Rest_printer_s = struct
+ (* : (module Pide_printer.Printer_spec) = (module struct*)
   let can_print = function
     | Feedback.Processed | Feedback.Message _ -> true
     | _ -> false
 
-  let status_element = Xml_datatype.Element(Coq_markup.statusN, [],
-    Coq_markup.status_finished)
-
-  let make_body = function
-    | Feedback.Processed -> status_element
+   let make_body = function
+    | Feedback.Processed -> Coq_markup.status_finished_element
     | Feedback.Message { Feedback.message_content = s } ->
         Xml_datatype.Element(Coq_markup.writelnN, [], Pide_xml.Encode.string s)
     | _ -> raise Pide_printer.Unhandled
@@ -21,18 +19,8 @@ let rest_printer : (module Pide_printer.Printer) = (module struct
   let children = function
     | Xml_datatype.Element(_, _, c) -> c
     | _ -> []
+end
 
-  let print_func id route content =
-    let position = Position.id_only id in
-    let body = make_body content in
-    if route = Feedback.default_route then
-      (* Main result *)
-      (output_function content) position  (children body)
-    else begin
-      (* Query result *)
-      Coq_output.result position route [status_element];
-      Coq_output.result position route [body]
-    end
-end)
-
-let () = Pide_document.install_printer rest_printer
+let () =
+  let module P = Pide_printer.Make_printer(Rest_printer_s) in
+  Pide_document.install_printer (module P)
