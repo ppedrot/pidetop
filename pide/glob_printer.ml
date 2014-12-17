f3d428fd203aadc17e88779297132aa08a36cdac
Carst Tankink <carst.tankink@inria.fr>
2014-12-17 14:20:00 +0100

    Port Glob_printer to Printer_spec.

--- pide/glob_printer.ml
+++ pide/glob_printer.ml
@@ -1,4 +1,4 @@
-let glob_printer : (module Pide_printer.Printer) = (module struct
+module Glob_printer : Pide_printer.Printer_spec = struct
   (* TODO: These definitions are yanked from the Coqdoc implementation.
    * It is probably a good idea to be more principled, and factor the functions
    * into some shared library with Coq...
@@ -49,8 +49,17 @@ let glob_printer : (module Pide_printer.Printer) = (module struct
   let def_map : (Loc.t * Coq_markup.entry_location) M.t ref = ref (M.empty)
 
   let lookup m k cont =
-    (try cont (M.find k !m)
-    with Not_found -> ())
+    (try Some (cont (M.find k !m))
+    with Not_found -> None)
+
+  let can_print = function
+    | Feedback.GlobRef _ | Feedback.FileLoaded _ | Feedback.GlobDef _ ->  true
+    | _ -> false
+
+  let output_function = function
+    | Feedback.GlobRef _ -> Coq_output.report
+    | Feedback.FileLoaded _ -> Coq_output.warning_msg ?props:None
+    | _ -> raise Pide_printer.Unhandled
 
   (* TODO: Basically the same as in tools/coqdoc/index.ml; except no refs. *)
   let load_globs (f: string) (id: int) =
@@ -68,27 +77,39 @@ let glob_printer : (module Pide_printer.Printer) = (module struct
                let typ = type_of_string ty in
                def_map := M.add (typ, name,  secpath) (loc, Coq_markup.ExtFile v_name) !def_map)
           with Scanf.Scan_failure _ | End_of_file -> ()
-        done
+        done;
+        assert false;
+        None
       with End_of_file ->
-        close_in c
+        close_in c;
+        None
     with Sys_error s ->
-      Coq_output.warning_msg (Position.id_only id)
+      Some (Xml_datatype.Element(Coq_markup.statusN, [],
         (Pide_xml.Encode.string ("Warning: " ^ glob_name ^
-         ": No such file or directory (links will not be available)"))
+         ": No such file or directory (links will not be available)"))))
 
-  let print_func id route = function
+  let make_pos = function
+    | Feedback.FileLoaded _ -> Position.id_only
+    | Feedback.GlobRef (loc, _, _ ,_, _) ->
+        Position.of_loc loc
+    | _ -> raise Pide_printer.Unhandled
+
+  let make_body id = function
   | Feedback.FileLoaded(dirname, filename) ->
       load_globs filename id
   | Feedback.GlobDef (loc, name, secpath, ty) ->
       let typ = type_of_string ty in
-      def_map := M.add (typ, name, secpath) (loc, Coq_markup.Local id) !def_map
+      def_map := M.add (typ, name, secpath) (loc, Coq_markup.Local id) !def_map;
+      None
   | Feedback.GlobRef (loc, _fp, mp, name, ty) ->
       let typ = type_of_string ty in
       lookup def_map (typ, name, mp) (fun (dest, dest_id) ->
-        let position = Position.of_loc loc id in
-        Coq_output.report position (Coq_markup.entity id loc dest_id dest name ty)
+        Xml_datatype.Element(Coq_markup.reportN, [],
+        (Coq_markup.entity id loc dest_id dest name ty))
       )
   | _ -> raise Pide_printer.Unhandled
-end)
+end
 
-let () = Pide_document.install_printer glob_printer
+let () =
+  let module P = Pide_printer.Make_printer(Glob_printer) in
+  Pide_document.install_printer (module P)
